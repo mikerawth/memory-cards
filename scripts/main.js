6aaea2bb88ceff4341280d8e6b026a95d40fd178
Michael Roth <michael.franklin.roth@gmail.com>
2022-11-04 06:38:43 -0400

    Initial Commit

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+$(document).ready(()=>{
+    console.log("ready")
+    
+    // DISPLAY: i # of cards
+    // for(let i = 0; i < 8; i++) {
+    //     $("#table").append(`<img class="card" id="${i}" src="images/card-back-red.png" alt="Playing card's back, red" />`)
+    // }
+
+    // FUNCTION / DEBUG: click on card
+    // $(".card").click(function(){
+    //     console.log(`You clicked on a card!`)
+    //     console.log(this.id)
+    // })
+
+})
