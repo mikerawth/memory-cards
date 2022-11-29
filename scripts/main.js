55a6c260cce1425fdf17cc86610a9bdc2627a981
Michael Roth <michael.franklin.roth@gmail.com>
2022-11-28 19:23:33 -0500

    cards are created through javascript

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,37 @@
 $(document).ready(()=>{
-    console.log("ready")
+    console.log("created with jQuery")
     
+
+    let rowAmount = 2;
+    let colAmount = 4;
+
+    for(let r = 0; r < rowAmount; r++) {
+        let rowId = `row-${r}`;
+        $("#table").append(`
+            <div id="${rowId}" class="row"></div>
+        `);
+        for(let c = 0; c < colAmount; c++) {
+            let colId = `col-${c}`
+            $(`#${rowId}`).append(`
+                <div id="${colId}">
+                    <div class="card face-down">
+                        <div class="card-inner">
+                            <span class="content">A</span>
+                        </div>
+                    </div>
+                </div>
+            `);
+        }
+    }
     // DISPLAY: i # of cards
-    // for(let i = 0; i < 8; i++) {
-    //     $("#table").append(`<img class="card" id="${i}" src="images/card-back-red.png" alt="Playing card's back, red" />`)
-    // }
+    for(let i = 0; i < 8; i++) {
+        // $("#table").append(`<img class="card" id="${i}" src="images/card-back-red.png" alt="Playing card's back, red" />`)
+    }
 
     // FUNCTION / DEBUG: click on card
-    // $(".card").click(function(){
-    //     console.log(`You clicked on a card!`)
-    //     console.log(this.id)
-    // })
+    $(".card").click(function(){
+        console.log(`You clicked on a card!`)
+        console.log(this.id)
+    })
 
 })
