c8cd2637ca0c10eb522d6495576b96b3e8ca8248
Michael Roth <michael.franklin.roth@gmail.com>
2022-12-01 18:09:55 -0500

    added shuffle(), comments

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,48 @@
 // temporary / for testing
 const DATA = ["A", "B", "C", "D"];
 const COLUMNS = 4;
-// 
 
+
+// credit to this code is from: 
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+}
+
+
+// takes in the data being used for the card values
+// duplicates each values
+// and then randomizes the order through shuffle()
 function createContent(data) {
     let content = []
     for(c of data) {
         content.push(c);
         content.push(c);
     }
+    content = shuffle(content);
+    // DEBUG:
+    console.log(`content after shuffle()\n ${content}`)
     return content;
 }
 
+// function for all of the html of the card
+// check style.css for information
+// the classes card, face-down, face-up and content 
+//   are all being used
 function cardHtmlToString(contentString) {
     cardHtml = 
     `
@@ -24,10 +55,13 @@ function cardHtmlToString(contentString) {
     return cardHtml;
 }
 
+// # of columns should be consistent
+// # of rows will depend upon the amount of content
 function getRows(content, columns) {
     return Math.ceil(content.length / columns);
 }
 
+// will display all of the cards on the table
 function displayCards(content, columns, rows) {
     let contentIndex = 0;
     for(let r = 0; r < rows; r++) {
