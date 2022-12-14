b9997a0275bcc5b0ec286df873a2c5c2fddc9ef1
Michael Roth <michael.franklin.roth@gmail.com>
2022-12-13 22:44:45 -0500

    some basics

--- scripts/card.js
+++ scripts/card.js
@@ -1,5 +1,28 @@
 class Card {
-    constructor(value) {
+    constructor(value,face) {
+        // value is used for comparing cards
         this.value = value;
+        // face determines if a card is face-down or face-up on the board
+        // stored as a BOOLEAN:
+        // TRUE = face-up
+        // FALSE = face-down
+        this.face = face;
+    }
+
+    getValue() {
+        return this.value;
+    }
+
+    getFace() {
+        return this.face;
+    }
+
+    displayCard(face){
+        if (face == True) {
+            // display card face-up
+        }
+        else {
+            // display card face-down
+        }
     }
 }
